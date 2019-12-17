dabbe72de51c5ae0eb8dad1be5248abb7eda2aaa
myfulltexts <458581726@qq.com>
2019-12-17 10:23:15 +0800

    totate me

---
+++ Runtime/RotateMe.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMe : MonoBehaviour
+{
+    public float angularVelocity = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        angularVelocity = 15.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rot = Quaternion.AngleAxis(angularVelocity * Time.deltaTime, Vector3.up);
+        transform.localRotation = rot * transform.localRotation;
+    }
+}
